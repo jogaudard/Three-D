3315f3c277521ea558185e674c5a430ff5414c60
Joseph Gaudard <jga051@bio3103914.klientdrift.uib.no>
2022-01-03 21:44:48 +0100

    new graph

--- R/C-flux/analysis2021.R
+++ R/C-flux/analysis2021.R
@@ -5,11 +5,11 @@ source("R/Rgathering/create meta data.R")
 
 
 get_file(node = "pk4bg",
-         file = "Three-D_c-flux_2021.csv",
+         file = "Three-D_c-flux_2021_corrected.csv",
          path = "data/C-Flux/summer_2021",
          remote_path = "C-Flux")
 
-flux <- read_csv("data/C-Flux/summer_2021/Three-D_c-flux_2021.csv") %>% 
+flux <- read_csv("data/C-Flux/summer_2021/Three-D_c-flux_2021_corrected.csv") %>% 
   mutate(
     corrected_flux = case_when( # discard data of low quality
       p.value <= 0.05 & r.squared >= 0.7 ~ corrected_flux,
@@ -69,9 +69,18 @@ flux2021 <- left_join(flux_gep, metaTurfID, by = "turfID") %>%
       "M" = "Medium",
       "N" = "Natural"
     )),
-    Nlevel = as.factor(Nlevel)
+    nitrogen = case_when(
+      Nlevel %in% c(1:3) ~ 0,
+      Nlevel == 4 ~ 0.5,
+      Nlevel == 5 ~ 1,
+      Nlevel == 6 ~ 5,
+      Nlevel == 7 ~ 10,
+      Nlevel == 8 ~ 50,
+      Nlevel == 9 ~ 100,
+      Nlevel == 10 ~ 150
+    )
   ) %>% 
-  select(turfID, type, comments, date, temp_soil, campaign, corrected_flux, site, block, grazing, warming, Nlevel) %>% 
+  select(turfID, type, comments, date, temp_soil, campaign, corrected_flux, site, block, grazing, warming, nitrogen) %>% 
   group_by(type, site, block, campaign, grazing) %>% #creating ID to connect transplant and ambient
   mutate(
     pairID = cur_group_id()
@@ -86,11 +95,16 @@ ggplot(flux2021, aes(x = temp_soil, y = corrected_flux, color = grazing)) +
   facet_grid(vars(type), vars(campaign), scales = "free") +
   ggsave("temp_soil_vs_flux_grazing.png", height = 20, width = 20, units = "cm")
 
-ggplot(flux2021, aes(x = temp_soil, y = corrected_flux, color = Nlevel)) +
+ggplot(flux2021, aes(x = temp_soil, y = corrected_flux, color = nitrogen)) +
   geom_point(size = 0.4) +
   # geom_line(aes(group = pairID)) +
   geom_smooth(method = "lm", se = FALSE, size = 0.5, fullrange = TRUE) +
   facet_grid(vars(type), vars(campaign), scales = "free") +
   ggsave("temp_soil_vs_flux_Nlevel.png", height = 20, width = 20, units = "cm")
   
+ggplot(flux2021, aes(x = nitrogen, y = corrected_flux, color = warming)) +
+  geom_point(size = 0.4) +
+  # geom_line(aes(group = pairID)) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE, size = 0.5, fullrange = TRUE) +
+  facet_grid(vars(type), vars(campaign), scales = "free")
   
