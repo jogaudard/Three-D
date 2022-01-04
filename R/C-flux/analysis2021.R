4c604f2491ca7f8736b69b0efa9d544a14c0b776
Joseph Gaudard <jga051@bio3103914.klientdrift.uib.no>
2022-01-04 15:35:51 +0100

    more graphs and standardized soil temperature

--- R/C-flux/analysis2021.R
+++ R/C-flux/analysis2021.R
@@ -14,19 +14,24 @@ flux <- read_csv("data/C-Flux/summer_2021/Three-D_c-flux_2021_corrected.csv") %>
     corrected_flux = case_when( # discard data of low quality
       p.value <= 0.05 & r.squared >= 0.7 ~ corrected_flux,
       p.value >= 0.05 & r.squared <= 0.7 ~ corrected_flux, #or should it be 0 in that case?
-      )
+      ),
+    PAR_corrected_flux = case_when( # discard data of low quality
+      p.value <= 0.05 & r.squared >= 0.7 ~ PAR_corrected_flux,
+      p.value >= 0.05 & r.squared <= 0.7 ~ PAR_corrected_flux, #or should it be 0 in that case?
+    )
   ) %>% 
   filter(
     type == "NEE" |
+      # type == "SoilR" |
       type == "ER" #we do not use the LRC data
   ) %>% 
-  select(turfID, type, comments, date, temp_soilavg, campaign, corrected_flux)
+  select(turfID, type, comments, date, temp_soilavg, campaign, PAR_corrected_flux, corrected_flux)
   
 
 # GEP
 flux_gep <- flux %>%
     # pivot_wider(names_from = type, values_from = PARavg, names_prefix = "PARavg_") %>% 
-  # select(!c(comments)) %>%
+  select(!c(PAR_corrected_flux)) %>%
   # select(campaign, turfID, date, type, corrected_flux) %>%
   pivot_wider(names_from = type, values_from = c(corrected_flux, temp_soilavg, comments)) %>% 
   rename(
@@ -51,7 +56,35 @@ flux_gep <- flux %>%
   ) %>% 
   select(!c(temp_soilavg_ER, temp_soilavg_NEE, comments_ER, comments_NEE))
 
+flux_gep_PAR <- flux %>% #to inlcude PAR-only corrected fluxes if we want to look at soil temperature
+  # pivot_wider(names_from = type, values_from = PARavg, names_prefix = "PARavg_") %>% 
+  select(date, turfID, type, PAR_corrected_flux) %>%
+  # select(campaign, turfID, date, type, corrected_flux) %>%
+  pivot_wider(names_from = type, values_from = PAR_corrected_flux) %>% 
+  # rename(
+  #   ER = PAR_corrected_flux_ER,
+  #   NEE = PAR_corrected_flux_NEE
+  # ) %>%
+  mutate(
+    GEP = NEE - ER
+  ) %>% 
+  pivot_longer(c(ER, NEE, GEP), names_to = "type", values_to = "PAR_corrected_flux") #%>% 
+  # mutate(
+  #   temp_soil = case_when(
+  #     type == "ER" ~ temp_soilavg_ER,
+  #     type == "NEE" ~ temp_soilavg_NEE,
+  #     type == "GEP" ~ rowMeans(select(., c(temp_soilavg_NEE, temp_soilavg_ER)), na.rm = TRUE)
+  #   ),
+  #   comments = case_when(
+  #     type == "ER" ~ comments_ER,
+  #     type == "NEE" ~ comments_NEE,
+  #     type == "GEP" ~ comments_NEE
+  #   )
+  # ) %>% 
+  # select(!c(temp_soilavg_ER, temp_soilavg_NEE, comments_ER, comments_NEE))
+
 flux2021 <- left_join(flux_gep, metaTurfID, by = "turfID") %>% 
+  left_join(flux_gep_PAR, by = c("turfID", "date", "type")) %>% 
   mutate(
     warming = str_replace_all(warming, c(
       "W" = "Transplant",
@@ -79,8 +112,9 @@ flux2021 <- left_join(flux_gep, metaTurfID, by = "turfID") %>%
       Nlevel == 9 ~ 100,
       Nlevel == 10 ~ 150
     )
+    # , nitrogen = as.factor(nitrogen)
   ) %>% 
-  select(turfID, type, comments, date, temp_soil, campaign, corrected_flux, site, block, grazing, warming, nitrogen) %>% 
+  select(turfID, type, comments, date, temp_soil, campaign, PAR_corrected_flux, corrected_flux, site, block, grazing, warming, nitrogen) %>% 
   group_by(type, site, block, campaign, grazing) %>% #creating ID to connect transplant and ambient
   mutate(
     pairID = cur_group_id()
@@ -88,23 +122,64 @@ flux2021 <- left_join(flux_gep, metaTurfID, by = "turfID") %>%
   ungroup()
 
 #graphing
-ggplot(flux2021, aes(x = temp_soil, y = corrected_flux, color = grazing)) +
+ggplot(flux2021, aes(x = temp_soil, y = PAR_corrected_flux, color = grazing)) +
   geom_point(size = 0.4) +
   # geom_line(aes(group = pairID)) +
   geom_smooth(method = "lm", se = FALSE, size = 0.5, fullrange = TRUE) +
   facet_grid(vars(type), vars(campaign), scales = "free") +
   ggsave("temp_soil_vs_flux_grazing.png", height = 20, width = 20, units = "cm")
 
-ggplot(flux2021, aes(x = temp_soil, y = corrected_flux, color = nitrogen)) +
+ggplot(flux2021, aes(x = temp_soil, y = PAR_corrected_flux, color = nitrogen)) +
   geom_point(size = 0.4) +
   # geom_line(aes(group = pairID)) +
   geom_smooth(method = "lm", se = FALSE, size = 0.5, fullrange = TRUE) +
   facet_grid(vars(type), vars(campaign), scales = "free") +
-  ggsave("temp_soil_vs_flux_Nlevel.png", height = 20, width = 20, units = "cm")
+  ggsave("temp_soil_vs_flux_nitrogen.png", height = 20, width = 20, units = "cm")
   
-ggplot(flux2021, aes(x = nitrogen, y = corrected_flux, color = warming)) +
+ggplot(flux2021, aes(x = nitrogen, y = PAR_corrected_flux, color = warming)) +
+  geom_point(size = 0.4) +
+  # geom_line(aes(group = pairID)) +
+  geom_smooth(method = "lm",
+              # formula = y ~ poly(x, 2),
+              se = FALSE, size = 0.5, fullrange = TRUE) +
+  facet_grid(vars(type), vars(campaign), scales = "free") +
+  ggsave("nitrogen_vs_flux_warming.png", height = 20, width = 20, units = "cm")
+
+
+ggplot(flux2021, aes(x = nitrogen, y = PAR_corrected_flux, color = warming)) +
   geom_point(size = 0.4) +
   # geom_line(aes(group = pairID)) +
   geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE, size = 0.5, fullrange = TRUE) +
   facet_grid(vars(type), vars(campaign), scales = "free")
-  
+
+ggplot(flux2021, aes(x = nitrogen, y = corrected_flux, color = grazing)) +
+  geom_point(size = 0.4) +
+  geom_smooth(method = "lm",
+              # formula = y ~ poly(x, 2),
+              se = FALSE, size = 0.5, fullrange = TRUE) +
+  facet_grid(vars(type), vars(campaign), scales = "free") +
+  labs(
+    # title = "",
+    caption = bquote(~CO[2]~'flux standardized at PAR = 300 mol/'*m^2*'/s and soil temperature = 15 °C'),
+    color = "Grazing treatment",
+    x = "Nitrogen addition [kg/ha/y]",
+    y = bquote(~CO[2]~'flux [mmol/'*m^2*'/h]')
+  ) +
+  ggsave("nitrogen_vs_flux_grazing_fixedtemp.png", height = 20, width = 20, units = "cm")
+
+ggplot(flux2021, aes(x = nitrogen, y = corrected_flux, color = warming)) +
+  geom_point(size = 0.4) +
+  geom_smooth(method = "lm",
+              # formula = y ~ poly(x, 2),
+              se = FALSE, size = 0.5, fullrange = TRUE) +
+  facet_grid(vars(type), vars(campaign), scales = "free") +
+  labs(
+    # title = "",
+    caption = bquote(~CO[2]~'flux standardized at PAR = 300 mol/'*m^2*'/s and soil temperature = 15 °C'),
+    color = "Grazing treatment",
+    x = "Nitrogen addition [kg/ha/y]",
+    y = bquote(~CO[2]~'flux [mmol/'*m^2*'/h]')
+  ) +
+  ggsave("nitrogen_vs_flux_warming_fixedtemp.png", height = 20, width = 20, units = "cm")
+
+       
